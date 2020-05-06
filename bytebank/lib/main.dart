8bc7ffd37f0a6db9415bbf5cabd5f797c43a83e2
IsDR520 <is_reis_sp@hotmail.com>
2020-05-06 15:02:40 -0300

    Ajuste do tema

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -7,9 +7,14 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
+      theme: ThemeData(
+        primaryColor: Colors.grey[500],
+        accentColor: Colors.greenAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.greenAccent[700],
+          textTheme: ButtonTextTheme.primary)
       ),
+      home: ListaTransferencias(),
     );
   }
 }
